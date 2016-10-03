3b0f49407fe6c61a9420fdeedbf2751e152a8757
Riekelt Brands <riekelturk@yahoo.nl>
2016-10-03 20:31:23 +0200

    init

---
+++ frmMain.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Radio10
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.wmpMain = new AxWMPLib.AxWindowsMediaPlayer();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.wmpMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wmpMain
+            // 
+            this.wmpMain.Enabled = true;
+            this.wmpMain.Location = new System.Drawing.Point(-2, 0);
+            this.wmpMain.Name = "wmpMain";
+            this.wmpMain.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpMain.OcxState")));
+            this.wmpMain.Size = new System.Drawing.Size(430, 69);
+            this.wmpMain.TabIndex = 0;
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Text = "Radio 10 Player";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseDoubleClick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 68);
+            this.Controls.Add(this.wmpMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Radio 10 Player";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wmpMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer wmpMain;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radio10
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            this.Resize += frmMain_Resize;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.trayIcon.Icon = this.Icon;
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                trayIcon.Visible = true;
+                this.ShowInTaskbar = false;
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                this.trayIcon.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        private void trayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            trayIcon.Visible = false;
+            this.ShowInTaskbar = true;
+        }
+    }
+}
